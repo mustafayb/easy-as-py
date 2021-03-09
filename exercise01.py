752cb2b57d46758bae96ab1ab0fabcd54b2dc591
mustafa <my@kali.kali>
2021-03-09 21:21:47 +0100

    oefening -1b, vervolg op oef-1a

--- exercise01.py
+++ exercise01.py
@@ -4,6 +4,15 @@ import os
 
 path_components = os.getcwd()
 
-lijst = path_components.split('/')
+path_components = path_components.split('/')
 
-print (lijst)
+path_components.remove('')
+
+print (path_components)
+
+print (path_components)
+
+def get_path_components():
+        return os.getcwd().split('/')
+
+print (get_path_components())
